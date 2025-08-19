a5f614fcdadc954a7d4b196375e8b350583dbd0c
Abhi <abhinav118@gmail.com>
2025-08-19 17:34:02 -0400

    Add components/CommentSection.tsx

---
+++ components/CommentSection.tsx
@@ -0,0 +1,12 @@
+interface CommentSectionProps {
+  nested?: any
+  sortable?: any
+}
+
+export default function CommentSection({ nested, sortable }: CommentSectionProps) {
+  return (
+    <div className="">
+      {/* CommentSection component content */}
+    </div>
+  )
+}
