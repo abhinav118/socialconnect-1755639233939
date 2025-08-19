abefe4d50f6163d363e2a07626314947d8a1b5dd
Abhi <abhinav118@gmail.com>
2025-08-19 17:34:02 -0400

    Add components/NavigationSidebar.tsx

---
+++ components/NavigationSidebar.tsx
@@ -0,0 +1,12 @@
+interface NavigationSidebarProps {
+  collapsible?: any
+  showNotificationBadge?: any
+}
+
+export default function NavigationSidebar({ collapsible, showNotificationBadge }: NavigationSidebarProps) {
+  return (
+    <div className="">
+      {/* NavigationSidebar component content */}
+    </div>
+  )
+}
