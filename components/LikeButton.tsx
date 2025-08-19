62e986204c11850049af42d219bf44cb25e2e7b1
Abhi <abhinav118@gmail.com>
2025-08-19 17:34:01 -0400

    Add components/LikeButton.tsx

---
+++ components/LikeButton.tsx
@@ -0,0 +1,12 @@
+interface LikeButtonProps {
+  animated?: any
+  showCount?: any
+}
+
+export default function LikeButton({ animated, showCount }: LikeButtonProps) {
+  return (
+    <div className="">
+      {/* LikeButton component content */}
+    </div>
+  )
+}
