1243025c1ac1c701ade2f290b0e9e8cba52d171c
Abhi <abhinav118@gmail.com>
2025-08-19 17:34:01 -0400

    Add components/UserAvatar.tsx

---
+++ components/UserAvatar.tsx
@@ -0,0 +1,13 @@
+interface UserAvatarProps {
+  size?: any
+  showOnlineStatus?: any
+  clickable?: any
+}
+
+export default function UserAvatar({ size, showOnlineStatus, clickable }: UserAvatarProps) {
+  return (
+    <div className="">
+      {/* UserAvatar component content */}
+    </div>
+  )
+}
