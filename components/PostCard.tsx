36236e5f7ddcfb30bf8d903f936853a69f73b3c0
Abhi <abhinav118@gmail.com>
2025-08-19 17:34:00 -0400

    Add components/PostCard.tsx

---
+++ components/PostCard.tsx
@@ -0,0 +1,15 @@
+interface PostCardProps {
+  showAuthor?: any
+  showTimestamp?: any
+  allowLike?: any
+  allowComment?: any
+  allowShare?: any
+}
+
+export default function PostCard({ showAuthor, showTimestamp, allowLike, allowComment, allowShare }: PostCardProps) {
+  return (
+    <div className="">
+      {/* PostCard component content */}
+    </div>
+  )
+}
